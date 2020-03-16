48d9196a73ce30bc97bb258a9e795b1c9084a14a
Emmanuel Adesile <emma2adesile@gmail.com>
2020-03-16 07:48:58 +0100

    Add profile for heroku deploy Add heroku-postbuild script Add env variable to production db

--- src/config/config.js
+++ src/config/config.js
@@ -1,33 +1,29 @@
 require('dotenv').config();
 
-const {
-  DB_NAME,
-  DB_NAME_TEST,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_HOST,
-  DB_URL_PROD,
-} = process.env;
-
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    port: DB_PORT,
-    host: DB_HOST,
-    dialect: 'postgres',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   },
   test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME_TEST,
-    port: DB_PORT,
-    host: DB_HOST,
-    dialect: 'postgres',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   },
   production: {
-    use_env_variable: DB_URL_PROD,
-  },
+    // username: process.env.DB_USERNAME_PROD,
+    // password: process.env.DB_PASSWORD_PROD,
+    // database: process.env.DB_NAME_PROD,
+    // port: process.env.DB_PORT_PROD,
+    // host: process.env.DB_HOST_PROD,
+    dialect: 'postgres',
+    use_env_variable: process.env.DB_URL_PROD
+  }
 };
